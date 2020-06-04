a3c6a9058ed8215e106ec38c9ccafd33299bcab4
Jeong <yojeo@a483e78929c2.ant.amazon.com>
2020-06-03 19:16:20 -0700

    final revisions done for final review

--- src/save_to_s3/app.py
+++ src/save_to_s3/app.py
@@ -5,7 +5,7 @@ import boto3
 s3_client = boto3.client('s3')
 
 def lambda_handler(event, context):
-    bucket_name = os.environ(['s3_bucket_name'])
+    bucket_name = os.environ['s3_bucket_name']
     build_name = event['detail']['build']['message']
     build_number = event['detail']['build']['number']
     key = build_name+str(build_number)+'/event_payload.json'
